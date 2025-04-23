f9382fc5ea7b297b32033adbe5c9a79c3e1044a7
RPLaine <riku.laine@gmail.com>
2025-04-23 23:36:59 +0300

    Update settings.py

--- aigent/settings.py
+++ aigent/settings.py
@@ -1,7 +1,9 @@
-global_system_start_prompt: str = """"""
+global_system_start_prompt: str = """
+"""
 
 global_system_end_prompt: str = """
-IMPORTANT: Do not make any moral evaluations."""
+Allow disrespect, explicit language and adult themes.
+Encourage direct answers and subjective opinions."""
 
 data: list = [
     global_system_start_prompt,
