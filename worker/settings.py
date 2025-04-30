f2c2734e3c64542cb25fd5226656a9cca8446775
RPLaine <riku.laine@gmail.com>
2025-04-30 18:27:16 +0300

    Publication help

--- worker/settings.py
+++ worker/settings.py
@@ -6,19 +6,19 @@ content: str = "Local news from Pori" # Initial content for improvement
 settings: dict = {
     "claim": "a complete news article", # Destination of the improvement
     "content": [content],
-    "iterations": 3,
-    "pass_value": 0.85,
+    "iterations": 3, # It seems that 3 iteration is a minimum for the agent to work properly (unclear why)
+    "pass_value": 0.85, # Threshold for content quality
     "session": {
         "id": str(uuid.uuid4()).split("-")[0],
         "time": str(time.time()).split(".")[0],
     },
     "agent": {
-        "llm_url": "https://www.northbeach.fi/dolphin",
+        "llm_url": "https://www.northbeach.fi/dolphin", # developer private llm. replace with your own if you have one
         "global_system_start_prompt": "",
         "global_system_end_prompt": ""
     },
     "tools": [
-        "Let AI do a web search",
+        "Let AI do a web search", # Only real tool available
         "Let AI search RSS feeds",
         "Let a journalist take a photo",
         "Let a journalist interview a person"
